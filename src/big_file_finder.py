e8179a7728f49d1b23130e1dd8a5c19e55a27160
yashLadha <yashladhapankajladha123@gmail.com>
2016-06-29 18:44:53 +0530

    added big file finder

---
+++ src/big_file_finder.py
@@ -0,0 +1,21 @@
+"""
+Code to Search your Preferred drive for
+files that are larger than 100 MB
+"""
+
+from os.path import getsize
+from os import walk
+from os.path import abspath
+from os.path import join
+from os import chdir
+
+
+def file_searcher(direct):
+    chdir(direct)
+    for foldername, subfolder, filename in walk(direct):
+        for file in filename:
+            if getsize(abspath(join(foldername, file)))/100 > 1000000:
+                print(abspath(join(foldername, file)))
+
+
+file_searcher('D:\\')
