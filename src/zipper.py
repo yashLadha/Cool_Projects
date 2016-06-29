445b247ed36b5729d5ed6cb3453e63de62bb0026
yashLadha <yashladhapankajladha123@gmail.com>
2016-06-29 15:05:03 +0530

    added zipper

---
+++ src/zipper.py
@@ -0,0 +1,29 @@
+"""
+Code to backup the contents of folder
+and create a zip file from that folder.
+"""
+
+from src.create_dir import move_folder
+from zipfile import ZipFile
+from os.path import abspath
+from os.path import basename
+from os.path import join
+from os import walk
+from os.path import pardir
+
+
+def back_zip(folder):
+    folder = abspath(folder)
+    backup_zip = ZipFile('Backup_' + basename(folder)+'.zip', 'w')
+    for foldername, subfoldees, filenames in walk(folder):
+        backup_zip.write(foldername)
+        for filename in filenames:
+            newBase = 'Backup_'+basename(folder)
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue
+            backup_zip.write(join(foldername, filename))
+    backup_zip.close()
+    move_folder(pardir+'\\'+'src'+'\\'+'Backup_' + basename(folder)+'.zip', 'D', 'Backup')
+
+
+back_zip(pardir)
