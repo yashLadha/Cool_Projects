5c651f5c6085a5024a5b6e0073a8bb56a783ca3b
yashLadha <yashladhapankajladha123@gmail.com>
2016-07-13 18:21:28 +0530

    added wallpaper download option in full hd

--- src/image grapper.py
+++ src/image grapper.py
@@ -1,4 +1,5 @@
 from os import chdir
+import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
@@ -9,6 +10,15 @@ from os.path import pardir
 from src.create_dir import create_directory
 
 GOOGLE_IMAGE = 'https://www.google.co.in/search?site=&tbm=isch&source=hp&biw=1920&bih=949&'
+WALLPAPERS_KRAFT = 'https://wallpaperscraft.com/search/keywords?'
+usr_agent = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
 
 
 def image_grabber(ch):
@@ -18,23 +28,56 @@ def image_grabber(ch):
         search_query = {'q': data}
         search = urlencode(search_query)
         print(search)
-        g = GOOGLE_IMAGE+search
-        request = Request(g, headers={'User-Agent': 'Mozilla/5.0'})
+        g = GOOGLE_IMAGE + search
+        request = Request(g, headers=usr_agent)
         r = urlopen(request).read()
         sew = BeautifulSoup(r, 'html.parser')
         count = 0
         for links in sew.find_all('img'):
-            urlretrieve(links.get('src'), 'img'+str(count)+'.jpg')
+            urlretrieve(links.get('src'), 'img' + str(count) + '.jpg')
             count += 1
         return True
 
     elif ch == 2:
+        cont = set()  # Stores the links of images
+        temp = set()  # Refines the links to download images
+
+        print('Enter data to download wallpapers: ')
+        data = input()
+        search_query = {'q': data}
+        search = urlencode(search_query)
+        print(search)
+        g = WALLPAPERS_KRAFT + search
+        request = Request(g, headers=usr_agent)
+        r = urlopen(request).read()
+        sew = BeautifulSoup(r, 'html.parser')
+        count = 0
+        for links in sew.find_all('a'):
+            if 'wallpaperscraft.com/download' in links.get('href'):
+                cont.add(links.get('href'))
+        for re in cont:
+            # print all valid links
+            # print('https://wallpaperscraft.com/image/' + re[31:-10] + '_' + re[-9:] + '.jpg')
+            temp.add('https://wallpaperscraft.com/image/' + re[31:-10] + '_' + re[-9:] + '.jpg')
+
+        # Goes to Each link and downloads high resolution images
+
+        for re in temp:
+            rs = requests.get(re)
+            with open('img' + str(count) + '.jpg', 'wb') as file:
+                file.write(rs.content)
+            # urlretrieve(re, 'img' + str(count) + '.jpg')
+            count += 1
+
+        return True
+
+    elif ch == 3:
         for folders, subfolder, files in walk(curdir):
             for folder in subfolder:
                 print(folder)
         return True
 
-    elif ch == 3:
+    elif ch == 4:
         print('Enter the directory to be set: ')
         data = input()
         chdir(data + ':\\')
@@ -43,7 +86,7 @@ def image_grabber(ch):
         create_directory(data)
         return True
 
-    elif ch == 4:
+    elif ch == 5:
         print(
             '''
 -------------------------***Thank You For Using***-------------------------
@@ -60,17 +103,17 @@ print(
     '''
 )
 create_directory('Images')
-DEFAULT_DIRECTORY = pardir+'\\Images'
+DEFAULT_DIRECTORY = pardir + '\\Images'
 chdir(DEFAULT_DIRECTORY)
-
 
 while run:
     print('''
 -------------------------WELCOME-------------------------
     1. Search for image
-    2. View Images in your directory
-    3. Set directory
-    4. Exit
+    2. Download Wallpapers 1080p
+    3. View Images in your directory
+    4. Set directory
+    5. Exit
 -------------------------*******-------------------------
     ''')
     choice = input()
