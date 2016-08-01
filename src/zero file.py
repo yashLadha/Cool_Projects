834c67203aaca177546607453a22dfc5ea3756fb
yashLadha <yashladhapankajladha123@gmail.com>
2016-08-01 11:54:33 +0530

    zero byte file finder added

---
+++ src/zero file.py
@@ -0,0 +1,21 @@
+"""
+Code to search for the folders whose size is
+Zero bytes
+"""
+
+from os.path import getsize
+from os import walk
+from os.path import abspath
+from os.path import join
+from os import chdir
+
+
+def file_searcher(direct):
+    chdir(direct)
+    for foldername, subfolder, filename in walk(direct):
+        for file in filename:
+            if getsize(abspath(join(foldername, file))) == 0:
+                print(abspath(join(foldername, file)))
+
+
+file_searcher('D:\\')
