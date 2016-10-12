cfc75e101a1748cce659fa9861c7c70f2525fe9c
Yash Ladha <yashladhapankajladha123@gmail.com>
2016-10-12 14:53:25 +0530

    added google search results

--- src/image grapper.py
+++ src/image grapper.py
@@ -4,24 +4,27 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from os import walk
+import json
 from os.path import curdir
 from urllib.request import urlretrieve
 from os.path import pardir
-from src.create_dir import create_directory
+from create_dir import create_directory
 
-GOOGLE_IMAGE = 'https://www.google.co.in/search?site=&tbm=isch&source=hp&biw=1920&bih=949&'
+GOOGLE_IMAGE = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
 WALLPAPERS_KRAFT = 'https://wallpaperscraft.com/search/keywords?'
 usr_agent = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive'
-}
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+        }
 
 
 def image_grabber(ch):
+
+    # Download images from google images
     if ch == 1:
         print('Enter data to download Images: ')
         data = input()
@@ -32,10 +35,19 @@ def image_grabber(ch):
         request = Request(g, headers=usr_agent)
         r = urlopen(request).read()
         sew = BeautifulSoup(r, 'html.parser')
-        count = 0
-        for links in sew.find_all('img'):
-            urlretrieve(links.get('src'), 'img' + str(count) + '.jpg')
-            count += 1
+        images = []
+        # print(sew.prettify())
+        results = sew.findAll("div",{"class":"rg_meta"})
+        for re in results:
+            link, Type = json.loads(re.text)["ou"] , json.loads(re.text)["ity"]
+            images.append(link)
+        counter = 0
+        for re in images:
+            rs = requests.get(re)
+            with open('img' + str(counter) + '.jpg', 'wb') as file:
+                file.write(rs.content)
+                # urlretrieve(re, 'img' + str(count) + '.jpg')
+                counter += 1
         return True
 
     elif ch == 2:
@@ -88,20 +100,20 @@ def image_grabber(ch):
 
     elif ch == 5:
         print(
-            '''
+                '''
 -------------------------***Thank You For Using***-------------------------
             '''
-        )
+            )
         return False
 
 
 run = True
 
 print(
-    '''
+        '''
 ***********[First Creating Folder To Save Your Images}***********
     '''
-)
+    )
 create_directory('Images')
 DEFAULT_DIRECTORY = pardir + '\\Images'
 chdir(DEFAULT_DIRECTORY)
